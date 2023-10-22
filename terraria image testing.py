da3795a90d2878be6b0a6b5d59a741223ce21d73
BenSisk <beepboopmoopjoop@gmail.com>
2023-10-22 20:50:42 +0100

    Threshold boss sprite
    Threshold the game screen to show only the boss (black-1) on the background (white-0).
    
    Only the red channel is used because a hunter potion is used, making all enemies appear red, while ignoring stuff like the cursor (made green).
    
    Blurring removes the stars (white) that would otherwise show up.
    
    Lag is a problem when this is combined with OCR, likely needs threading but this can wait.

--- terraria image testing.py
+++ terraria image testing.py
@@ -10,7 +10,6 @@ bounding_box = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
 sct = mss()
 
 def ocr(img):
-    img = cv2.resize(img, None, fx = 3, fy = 3)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)[1]
 
@@ -26,6 +25,18 @@ while True:
     player_hp_image = screen_cap[0:28, 1655:1767]
     print("Player: " + ocr(player_hp_image))
     print("Boss:" + ocr(boss_hp_image))
+
+    looky_here_area = screen_cap[160:970]
+
+    blue, green, red, not_sure = cv2.split(looky_here_area)
+
+    red = cv2.blur(red, (10, 10))
+    red = cv2.threshold(red, 50, 255, cv2.THRESH_BINARY)[1]
+
+    red = cv2.bitwise_not(red)
+    red = cv2.resize(red, (80, 45))
+
+    cv2.imshow('screen', red)
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
