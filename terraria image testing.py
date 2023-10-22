0835ff64abec52eecbdbcb07ce5b3585cdda3b8f
BenSisk <beepboopmoopjoop@gmail.com>
2023-10-22 18:34:24 +0100

    Read Player HP
    OCR now reads player HP too. Moved to function to reduce code duplication.

--- terraria image testing.py
+++ terraria image testing.py
@@ -9,21 +9,23 @@ bounding_box = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
 
 sct = mss()
 
+def ocr(img):
+    img = cv2.resize(img, None, fx = 3, fy = 3)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)[1]
+
+    ocr_result = pytesseract.image_to_string(img, lang='eng', \
+           config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789/')
+
+    return ocr_result
+
 while True:
     sct_img = sct.grab(bounding_box)
-    cv2_image = np.array(sct_img)
-    cropped_image = cv2_image[995:1020, 850:1070]
-
-    # cropped_image = cv2.GaussianBlur(cropped_image, (1, 1), 0)
-    cropped_image = cv2.resize(cropped_image, None, fx = 2, fy = 2)
-    cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-    cropped_image = cv2.threshold(cropped_image, 150, 255, cv2.THRESH_BINARY)[1]
-
-    ocr_result = pytesseract.image_to_string(cropped_image, lang='eng', \
-           config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789/')
-    print(ocr_result)
-
-    cv2.imshow('screen', cropped_image)
+    screen_cap = np.array(sct_img)
+    boss_hp_image = screen_cap[995:1020, 850:1070]
+    player_hp_image = screen_cap[0:28, 1655:1767]
+    print("Player: " + ocr(player_hp_image))
+    print("Boss:" + ocr(boss_hp_image))
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
