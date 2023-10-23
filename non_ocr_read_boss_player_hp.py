feaa44ab5b853c3b467e5a44787a16d0d0750176
BenSisk <beepboopmoopjoop@gmail.com>
2023-10-23 15:26:35 +0100

    Working non-ocr hp bars
    Returns an index value (essentially a percentage) for the hp/100 remaining on either the player or the boss.
    
    Default hp drains from right to left but player hp is left to right, requiring it be first reversed via np.flip()

--- non_ocr_read_boss_player_hp.py
+++ non_ocr_read_boss_player_hp.py
@@ -2,6 +2,21 @@ import numpy as np
 import cv2
 from mss import mss
 
+def check_hp_bar(image, flipped=False):
+    _, _, red, _ = cv2.split(image)
+
+    red = cv2.blur(red, (5, 5))
+    red = cv2.threshold(red, 50, 255, cv2.THRESH_BINARY)[1]
+    red = cv2.resize(red, (100, 1))
+
+
+    index = 0
+    for pixel in red[0, :]:
+        if (pixel == 0):
+            break
+        index += 1
+
+    return index
 
 bounding_box = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
 sct = mss()
@@ -9,28 +24,26 @@ while True:
     sct_img = sct.grab(bounding_box)
     screen_cap = np.array(sct_img)
 
+    boss_alive = False
+    player_alive = False
 
     if cv2.threshold(screen_cap[984, 617], 150, 255, cv2.THRESH_BINARY)[1][0][0] == 255:
-        print("yes")
+        boss_alive = True
+        # print("Boss is alive")
 
-    looky_here_area = screen_cap[995:1015, 655:1275]
+    if screen_cap[45, 1834, 2] > 150:
+        player_alive = True
+        print("Player is alive")
+    else:
+        print("Player is dead")
 
-    blue, green, red, _ = cv2.split(looky_here_area)
+    if(boss_alive):
+        boss_hp = check_hp_bar(screen_cap[995:1015, 655:1275])
+    
+    player_hp_bar = check_hp_bar(np.flip(screen_cap[40:50, 1500:1834]))
 
-    red = cv2.blur(red, (5, 5))
-    red = cv2.threshold(red, 50, 255, cv2.THRESH_BINARY)[1]
-
-    # red = cv2.bitwise_not(red)
-    red = cv2.resize(red, (100, 10))
-
-    index = 0
-    for pixel in red[5, :]:
-        if (pixel == 0):
-            # print(index)
-            break
-        index += 1
-
-    cv2.imshow('screen', red)
+    # print("Boss HP: " + str(boss_hp))
+    # print("Player HP: " + str(player_hp_bar))
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
